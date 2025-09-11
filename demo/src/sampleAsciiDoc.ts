262be54cad5489ba5af2f69d99cbd89d139129f5
akaguny <akaguny@users.noreply.github.com>
2025-09-11 21:15:35 +0300

    sample asciidoc based on the https://docs.asciidoctor.org/asciidoc/latest/text/

--- demo/src/sampleAsciiDoc.ts
+++ demo/src/sampleAsciiDoc.ts
@@ -1,34 +1,57 @@
 // Sample AsciiDoc content
-export const sampleAsciiDoc = `AsciiDoc to ProseMirror Demo
+export const asciidocTextFormattingandPunctuation = `== Bold https://docs.asciidoctor.org/asciidoc/latest/text/bold/
 
-This is a second paragraph.
+A bold *word*, and a bold *phrase of text*.
 
-* First item
-* Second item with multiple paragraphs
+Bold c**hara**cter**s** within a word.
 
-  This is the second paragraph in the list item.
 
-  And here's another paragraph with *bold* text.
-* Third item
+== Italic https://docs.asciidoctor.org/asciidoc/latest/text/italic/
 
-1. First ordered item
-2. Second ordered item with nested list
+An italic _word_, and an italic _phrase of text_.
 
-  This item has a paragraph and a nested list:
+Italic c__hara__cter__s__ within a word.
 
-  ** Sub item 1
-  ** Sub item 2
-3. Third ordered item
+== Monospace https://docs.asciidoctor.org/asciidoc/latest/text/monospace/
 
-Here's a code block:
+"\`Wait!\`" Indigo plucked a small vial from her desk's top drawer
+and held it toward us.
+The vial's label read: \`E=mc^2^\`; the \`E\` represents _energy_,
+but also pure _genius!_
 
-----
-console.log('Hello, world!');
-for (let i = 0; i < 10; i++) {
-  console.log(i);
-}
-----
+== Literal Monospace https://docs.asciidoctor.org/asciidoc/latest/text/literal-monospace/
 
-Check out this link:http://example.com[Example Link] for more information.
+You can reference the value of a document attribute using
+the syntax \`+{name}+\`, where \`name\` is the attribute name.
 
-And a third paragraph with multiple lines.`;
+== Highlight https://docs.asciidoctor.org/asciidoc/latest/text/highlight/
+
+Mark my words, #automation is essential#.
+
+== Quotation Marks and Apostrophes https://docs.asciidoctor.org/asciidoc/latest/text/quotation-marks-and-apostrophes/
+
+==# Single and double quotation mark syntax
+
+In Ruby, '\n' represents a backslash followed by the letter n.
+Single quotes prevent escape sequences from being interpreted.
+In contrast, "\n" represents a newline.
+
+== Subscript and Superscript
+
+=== Subscript and superscript syntax
+
+"\`Well the H~2~O formula written on their whiteboard could be part
+of a shopping list, but I don't think the local bodega sells
+E=mc^2^,\`" Lazarus replied.
+
+=== Superscript syntax that contains spaces
+
+The deepest body of water is Deep Creek Lake.^[citation{sp}needed]^
+
+== Edge cases https://docs.asciidoctor.org/asciidoc/latest/text/troubleshoot-unconstrained-formatting/
+
+=== A monospace phrase inside curved quotation marks
+
+"\`\`\`end points\`\`\`"
+`
+;
